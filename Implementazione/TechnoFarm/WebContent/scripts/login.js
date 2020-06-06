90446b3266015ec70b16d3242e1fd1e7c35a6b51
Luigi <luigibozzoli@studenti.unisa.it>
2020-06-06 17:41:59 +0200

    updated login

--- Implementazione/TechnoFarm/WebContent/scripts/login.js
+++ Implementazione/TechnoFarm/WebContent/scripts/login.js
@@ -17,13 +17,25 @@ function validateForm() {
 	var bool = new Boolean(emailUtente == email && password == passwordUtente);
 	alert(bool);
 	
-	if(bool){
+	if(bool == true){
 		alert("vero");
 		return true;
 	}else{
 		avvisa("Email o password errate");
 		alert("falso");
+		setTimeout(function(){ 
+
+	        alert('hello');
+	        location.replace("https://www.w3schools.com");
+	    }, 3000);  
+		
 		return false;
 	}
+}
+
+function avvisa(x){
+	$("#errore").text(x);
+	$("#errore").fadeIn();
+	$("#errore").delay(3000).fadeOut();;
 }
 
