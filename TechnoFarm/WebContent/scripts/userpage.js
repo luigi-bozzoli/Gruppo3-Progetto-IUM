96d3cb4b72c8828333e8fc9b730a8546065b3fde
Luigi <luigibozzoli@studenti.unisa.it>
2020-06-20 12:46:50 +0200

    bugs

--- TechnoFarm/WebContent/scripts/userpage.js
+++ TechnoFarm/WebContent/scripts/userpage.js
@@ -1,8 +1,17 @@
-/**
- * 
- */
-var email;
-var password;
+var emailU;
+var passwordU;
+
+
+function logout(){
+	
+	localStorage['login'] = 'false';
+	location.replace("index.html");
+}
+
+function leMieSerre(){
+	location.replace("index.html");
+}
+
 
 function mostraBottoni(){
 	$('#modificaEmail').css('display', 'block');
@@ -10,22 +19,20 @@ function mostraBottoni(){
 	$('#confermaModifiche').css('display', 'inline');
 	$('#liEmail').css('display', 'none');
 	$('#liPassword').css('display', 'none');
-	
+
 }
 
 function confermaDati(){
-	var email =$("#inputEmail").val();
-	var password = $("#inputPassword").val();
-	
-	$("#emailUtente").text(email);
-	$("#passwordUtente").text(password);
-	
+	var emailU =$("#inputEmail").val();
+	var passwordU = $("#inputPassword").val();
+
+	$("#emailUtente").text(emailU);
+	$("#passwordUtente").text(passwordU);
+
 	$('#modificaEmail').css('display', 'none');
 	$('#modificaPassword').css('display', 'none');
 	$('#confermaModifiche').css('display', 'none');
 	$('#liEmail').css('display', 'block');
 	$('#liPassword').css('display', 'block');
-	
-	
-}
 
+}
