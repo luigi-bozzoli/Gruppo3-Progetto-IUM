4e29b18454dcd0aa3c21ed887241d4eab6cf72fa
Lucio-Squitieri <55919285+Lucio-Squitieri@users.noreply.github.com>
2020-06-21 17:33:59 +0200

    Update login.js

--- TechnoFarm/WebContent/scripts/login.js
+++ TechnoFarm/WebContent/scripts/login.js
@@ -1,63 +1,60 @@
 /**
- * 
+ *
  */
 const email = "test";
 const password = "password";
 
 function check(bool){
-	var login = localStorage['login'];
-	if(login == null || login == 'false'){
-		return true;
-	}else{
-		if(bool == true)
-			location.replace("userPage.html");
-		else
-			location.replace("https://fontawesome.com/v4.7.0/icon/pagelines");
-	}
+    var login = localStorage['login'];
+    if(login == null || login == 'false'){
+        return true;
+    }else{
+        if(bool == true)
+            location.replace("userPage.html");
+        else
+            location.replace("LeMieSerre.html");
+    }
 }
 
 window.onload = function() {
-	checkLogin();
+    checkLogin();
 }
 
 function checkLogin(){
-	var login = localStorage['login'];
-	if(login == null || login == 'false'){
-		//return true;
-	}else{
-		$("#leMieSerre").css("display", "inline");
-		$("#bottoneRegistrazione").css("display", "none");
-	}
+    var login = localStorage['login'];
+    if(login == null || login == 'false'){
+        //return true;
+    }else{
+        $("#leMieSerre").css("display", "inline");
+        $("#bottoneRegistrazione").css("display", "none");
+    }
 }
 
 function validateForm() {
 
 
-	var emailUtente =$("#email").val();
-	var passwordUtente = $("#password").val();
+    var emailUtente =$("#email").val();
+    var passwordUtente = $("#password").val();
 
-	var bool = new Boolean(emailUtente == email && password == passwordUtente);
+    var bool = new Boolean(emailUtente == email && password == passwordUtente);
 
-	if(bool == true){
-		location.replace("userPage.html");
-		localStorage['login'] = 'true';
+    if(bool == true){
+        location.replace("LeMieSerre.html");
+        localStorage['login'] = 'true';
 
-		return true;
-	}else{
-		avvisa("Email o password errate");
-		/*setTimeout(function(){ 
+        return true;
+    }else{
+        avvisa("Email o password errate");
+        /*setTimeout(function(){
+            alert('hello');
+        }, 3000);  */
 
-	        alert('hello');
-
-	    }, 3000);  */
-
-		return false;
-	}
+        return false;
+    }
 }
 
 function avvisa(x){
-	$("#errore").text(x);
-	$("#errore").fadeIn();
-	$("#errore").delay(3000).fadeOut();
+    $("#errore").text(x);
+    $("#errore").fadeIn();
+    $("#errore").delay(3000).fadeOut();
 }
-
